8d3e1ee5e7a67e52111e39e2cfc369ecf3c161ef
itdev-lenovo <it1devops@gmail.com>
2020-04-08 18:30:17 +0530

    files added

---
+++ OverviewOfJavaLogging/JavaLogging/src/com/java/logging/javaLogging.java
@@ -0,0 +1,12 @@
+package com.java.logging;
+
+public class javaLogging {
+/*
+ * 1. Create a JavaEE application and Configuring the Glassfish logging service.
+ * 2.Create a JavaEE application and add a logger to the application.
+ * 3.Create a JavaEE application and Configuring the Glassfish logging service and
+     add a logger to the application.
+
+ * 
+ */
+}
---
+++ OverviewofJavaEEContainersandComponents/ContainersComponents/src/com/java/containers/Module2.java
@@ -0,0 +1,12 @@
+package com.java.containers;
+
+public class Module2 {
+ /*
+  * 1.Install the Glassfish server from IDE
+    2. Create a simple java bean. 
+    3. Create a JavaEE application and run on the local server display the results.
+    4.Create a JavaEE application, also create a simple java bean 
+      class and run on the local server display the results.
+  * 
+  */
+}
